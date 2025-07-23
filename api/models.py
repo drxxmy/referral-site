acd7e69fb6d75575d18d19597df2639d6cddb1f2
Vladislav <118668125+drxxmy@users.noreply.github.com>
2025-07-23 22:44:02 +0500

    Create UserManager model

--- api/models.py
+++ api/models.py
@@ -1,8 +1,23 @@
 import secrets
 import string
 
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError("Необходимо указать номер телефона")
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, phone_number, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(phone_number, password, **extra_fields)
 
 
 class User(AbstractBaseUser):
