1b38eb7efdb926168e36dc12874da837b781b47e
Vladislav <118668125+drxxmy@users.noreply.github.com>
2025-07-23 18:39:22 +0500

    Add User model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
+import secrets
+import string
+
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    phone_number = models.CharField(max_length=11, unique=True)
+    invite_code = models.CharField(max_length=6, unique=True, blank=True)
+    activated_invite = models.ForeignKey(
+        "self", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    created_at = models.DateField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # Генерация 6-значного инвайт кода
+        alphabet = string.ascii_letters + string.digits
+        self.invite_code = "".join(secrets.choice(alphabet) for _ in range(6))
+        return super().save(*args, **kwargs)
