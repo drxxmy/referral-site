b71928b99e41fd68d15c1ad9e79dcebe49e7646c
Vladislav <118668125+drxxmy@users.noreply.github.com>
2025-07-25 17:10:32 +0500

    Use PostgreSQL instead of SQLite

--- core/settings.py
+++ core/settings.py
@@ -91,10 +91,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "db",
+        "USER": "admin",
+        "PASSWORD": "123",
+        "HOST": "vladevslav.pythonanywhere.com",
+        "PORT": 5432,
+    },
 }
 
 
